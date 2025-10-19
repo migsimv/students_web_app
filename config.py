b855cba82c897ddc86caa5b26c1c56fa701d96de
Migle Simaviciute <simmigle8@gmail.com>
2025-10-19 12:43:46 +0300

    config adjusted for vercel/aws/local

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ import os
 
 class Config:
    # print(os.environ)
-    if 'RDS_HOSTNAME' in os.environ:
-       SQLALCHEMY_DATABASE_URI = (
+    on_vercel = os.getenv("VERCEL") is not None
+    on_aws = "RDS_HOSTNAME" in os.environ
+
+    if on_aws:
+        SQLALCHEMY_DATABASE_URI = (
             "postgresql://{user}:{password}@{host}:{port}/{db}".format(
                 user=os.environ["RDS_USERNAME"],
                 password=os.environ["RDS_PASSWORD"],
@@ -12,10 +15,13 @@ class Config:
                 db=os.environ["RDS_DB_NAME"],
             )
         )
+
+    elif on_vercel:
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
     else:
         SQLALCHEMY_DATABASE_URI = os.getenv(
             "DATABASE_URL",
             "postgresql://postgres:password@localhost:5432/studentsdb"
         )
-            
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
